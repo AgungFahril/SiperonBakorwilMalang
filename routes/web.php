4620471cd3c0df6987aef7dfc71ada745123e99f
oktavianafrista10 <oktavianafristaelliza@gmail.com>
2026-09-21 22:13:59 +0700

    perbaiki tampilan frontend admin

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('admin.dashboard');
+    });
+    Route::get('/peminjaman', function () {
+        return view('admin.peminjaman');
+    });
+    Route::get('/ruangan', function () {
+        return view('admin.ruangan');
+    });
+    Route::get('/pengguna', function () {
+        return view('admin.pengguna');
+    });
+    Route::get('/pengaturan', function () {
+        return view('admin.pengaturan');
+    });
+});
